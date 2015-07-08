efe098b192e8cea4c086da601b2aefc8f3427f64
Nikolai R Kristiansen <nikolaik@gmail.com>
2015-07-08 15:53:04 +0200

    User search fixes

--- apps/kassa/forms.py
+++ apps/kassa/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 from django.utils.translation import ugettext_lazy as _
 
+
 class SearchUserForm(forms.Form):
-    query = forms.CharField(required=False)  # ,label=_('Search existing members'))
+    query = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': _('Search')})
+    )
+
 
 class AddCardForm(forms.Form):
     phone_number = PhoneNumberField(widget=forms.TextInput(attrs={'type': 'tel'}))
--- static/dist/scripts/app.js
+++ static/dist/scripts/app.js
@@ -6,21 +6,27 @@ function format_results(results) {
         var label = '';
         var is_member = res.is_member && res.is_member === "1";
         var add_to_button = '';
-        var css_classes = '';
+        var css_classes = 'list-group-item search-result';
         var expires = res.expires;
 
         if(is_member) {
             label = ' <span class="label label-success">Member</span> ';
-            css_classes += 'member';
+            css_classes += ' member';
         } else if(expires === '0000-00-00') {
-            add_to_button = '<button class="btn btn-primary">Use</button> ';
+            add_to_button = '<input type="radio" name="random" id="'+res.id+'"> ';
             label = ' <span class="label label-default">Registered</span> ';
+            css_classes += ' registered';
         } else {
-            add_to_button = '<button class="btn btn-primary">Use</button> ';
+            add_to_button = '<input type="radio" name="random" id="'+res.id+'"> ';
             label = ' <span class="label label-warning">Expired: '+ expires +'</span> ';
+            css_classes += ' expired';
         }
-        var q = $('#id_query').val();
-        html += '<a href="?q='+q+'&phone_number='+ res.number +'" data-phone-number="'+ res.number +'" class="list-group-item search-result '+css_classes+'">' + add_to_button + res.firstname + ' ' + res.lastname + label + '</a>';
+
+        if(is_member) {
+            html += '<div class="'+css_classes+'">' + add_to_button + res.firstname + ' ' + res.lastname + label + '</div>';
+        } else {
+            html += '<label for="'+ res.id +'" data-phone-number="'+ res.number +'" class="'+css_classes+'">' + add_to_button + res.firstname + ' ' + res.lastname + label + '</label>';
+        }
     }
     return html + '</div>';
 }
@@ -50,9 +56,11 @@ $(document).ready(function(){
     });
 
     /* Click search result */
-    _dom.results.on('click', '.search-result', function(e){
-        e.preventDefault();
-        var number = $(this).attr('data-phone-number');
+    _dom.results.on('click', '.search-result input', function(e){
+        var label = $(this).parent();
+        var number = label.attr('data-phone-number');
+        $('.search-result').removeClass('selected');
+        label.toggleClass('selected');
         _dom.phoneNumber.val(number);
     });
 });
