a67434117fd54ba5bcc21aadaaa9251a20116960
Nikolai R Kristiansen <nikolaik@gmail.com>
2015-08-12 16:34:13 +0200

    Admin: Make events searchable

--- apps/kassa/admin.py
+++ apps/kassa/admin.py
@@ -5,5 +5,6 @@ from django.contrib import admin
 class KassaEventAdmin(admin.ModelAdmin):
     list_display = ['id', 'event', 'card_number', 'user_phone_number', 'user_inside_id', 'created']
     list_filter = ['event']
+    search_fields = ['card_number', 'user_phone_number', 'user_inside_id']
 
 admin.site.register(KassaEvent, KassaEventAdmin)
