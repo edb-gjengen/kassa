02701cb1c0b432aad339dd90d421917bd681bc67
Nikolai Kristiansen <nikolaik@gmail.com>
2015-08-25 11:30:51 +0200

    Adds refund event

---
+++ apps/kassa/migrations/0004_auto_20150825_1130.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kassa', '0003_auto_20150812_1530'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='kassaevent',
+            name='event',
+            field=models.CharField(max_length=255, choices=[('new_card_membership', 'Added new card membership (no user yet)'), ('sms_card_notify', 'Notified phone number about activation'), ('update_card', 'Updated user card number'), ('add_or_renew', 'Added or renewed membership user membership'), ('refund', 'Refunded membership')]),
+        ),
+    ]
