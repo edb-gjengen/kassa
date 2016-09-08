5ba8635592f69155810664d5c0a1075976ac091b
Nikolai Kristiansen <nikolaik@gmail.com>
2016-09-08 23:44:27 +0200

    feat: allow 1 month advance renewals

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -352,11 +352,13 @@ $(document).ready(function(){
             }
             var card = data.card;
             var user = data.user;
+            var today_plus_one_month = moment().add(1, 'month');
+
             if(!card) {
                 cardForm.fields.cardNumber = false;
                 set_field_state(_dom.cardNumberField, 'error', 'Cannot find card number in database.');
             }
-            else if(card && card.registered !== "" && user === null && moment(card.expires) <= moment()) {
+            else if(card && card.registered !== "" && user === null && moment(card.expires) <= today_plus_one_month) {
                 /* An allready registered card (with no user) can repurchase membership if expired */
                 cardForm.fields.cardNumber = true;
                 set_field_state(_dom.cardNumberField, 'success');
